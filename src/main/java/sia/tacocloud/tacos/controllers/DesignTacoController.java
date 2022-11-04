d0aadac639402e814539ec2869b86ba00be882de
Valetin Belousow <bel2002v@yandex.ru>
2022-11-04 19:41:32 +0300

    Order form and OrderController

--- src/main/java/sia/tacocloud/tacos/controllers/DesignTacoController.java
+++ src/main/java/sia/tacocloud/tacos/controllers/DesignTacoController.java
@@ -3,10 +3,7 @@ package sia.tacocloud.tacos.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import sia.tacocloud.tacos.models.Ingredient;
 import sia.tacocloud.tacos.models.Ingredient.Type;
 import sia.tacocloud.tacos.models.Taco;
@@ -58,6 +55,14 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco,@ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}",taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(
